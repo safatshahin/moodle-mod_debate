28c25f927012badc6158a62b2bc10ee5fa97c453
“Safat <safat@ecreators.com.au>
2021-04-05 14:10:05 +1000

    unit test start

---
+++ tests/generator/lib.php
@@ -0,0 +1,50 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * mod_debate data generator
+ *
+ * @package     mod_debate
+ * @copyright   2021 Safat Shahin <safatshahin@gmail.com>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+class mod_debate_generator extends testing_module_generator {
+    /**
+     * Creates a mod_debate instance
+     *
+     * @param array $record
+     * @param array $options
+     * @return StdClass
+     */
+    public function create_instance($record = null, array $options = null) {
+        $record = (object)(array)$record;
+
+        $defaultsettings = array(
+
+        );
+
+        foreach ($defaultsettings as $name => $value) {
+            if (!isset($record->{$name})) {
+                $record->{$name} = $value;
+            }
+        }
+
+        return parent::create_instance($record, (array)$options);
+    }
+}
